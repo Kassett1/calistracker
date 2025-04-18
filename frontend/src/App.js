19c4f16901a6b81e6f69eaac5220b112047ec5ae
Kassett1 <killian.touchard1@gmail.com>
2025-04-18 23:48:47 +0200

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-color4'>Test</h1>
+      <p className='text-red-500'>Test</p>
+    </div>
+  );
+}
+
+export default App;
