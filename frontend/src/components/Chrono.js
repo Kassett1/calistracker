a911053bd7a180ea603e1c89050c277210ae859e
Kassett1 <killian.touchard1@gmail.com>
2025-05-09 16:06:32 +0200

    début chronomètre

--- frontend/src/components/Chrono.js
+++ frontend/src/components/Chrono.js
@@ -2,8 +2,59 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 function Chrono() {
+  const [state, setState] = useState("initial");
+
+  const [checkpoints, setCheckpoints] = useState([]);
+
+  const handleStart = () => {
+    setState("running");
+  };
+
+  const handleStop = () => {
+    setState("stopped");
+  };
+
+  const handleLap = () => {};
+
+  const handleReset = () => {
+    setState("initial");
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen px-[10vw] gap-[10vh]">
+      {/* Affichage du chrono */}
+      <div className="text-5xl font-luckiest">00:00:00:00</div>
+
+      {/* Boutons */}
+      <div className="flex justify-center w-full gap-[10vw]">
+        {state === "stopped" && (
+          <button onClick={handleReset} className="transition-all duration-300">
+            <img src="/icones/reset.svg" alt="Reset" className="h-[15vw]" />
+          </button>
+        )}
+
+        {(state === "initial" || state === "stopped") && (
+          <button onClick={handleStart} className="transition-all duration-300">
+            <img src="/icones/play.svg" alt="Play" className="h-[15vw]" />
+          </button>
+        )}
+
+        {state === "running" && (
+          <button onClick={handleLap} className="transition-all duration-300">
+            <img
+              src="/icones/checkpoint.svg"
+              alt="Checkpoint"
+              className="h-[15vw]"
+            />
+          </button>
+        )}
+
+        {state === "running" && (
+          <button onClick={handleStop} className="transition-all duration-300">
+            <img src="/icones/pause.svg" alt="Pause" className="h-[15vw]" />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
