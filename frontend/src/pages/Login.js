512167e56572c48ec3246f1d88bd44813c377492
Kassett1 <killian.touchard1@gmail.com>
2025-06-04 14:09:26 +0200

    css page login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -58,14 +58,14 @@ function Login() {
 
   return (
     <>
-      <div>
-        <div>
+      <div className="flex items-center justify-center h-screen flex-col gap-[10vh] mx-[10vw]">
+        <div className="flex justify-center gap-[5vw]">
           <button
             onClick={handleLogin}
             className={`
               rounded-[10px] px-[3vw] py-[0.5vh]
               border-t-[2px] border-l-[2px] border-b-[4px] border-r-[4px] border-color5
-              font-cabin text-l
+              font-cabin text-lg
               transition-colors duration-200 transition-transform duration-100 active:scale-95
               ${mode === "login" ? "bg-accent1" : "bg-accent2"}
           `}
@@ -78,7 +78,7 @@ function Login() {
             className={`
               rounded-[10px] px-[3vw] py-[0.5vh]
               border-t-[2px] border-l-[2px] border-b-[4px] border-r-[4px] border-color5
-              font-cabin text-l
+              font-cabin text-lg
               transition-colors duration-200 transition-transform duration-100 active:scale-95 
               ${mode === "signup" ? "bg-accent1" : "bg-accent2"}
             `}
@@ -86,13 +86,26 @@ function Login() {
             S'inscrire
           </button>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">Email :</label>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-[3vh] items-center"
+        >
+          <div className="flex gap-[1vw] items-center">
+            <label htmlFor="email" className="font-cabin text-lg">
+              Email :
+            </label>
             <input
               type="mail"
               name="email"
               id="email"
+              className="bg-color1 rounded-[10px] px-[1vw] py-[0.5vh]
+              border-t-[2px] border-l-[2px] border-b-[4px] border-r-[4px] border-color5
+              font-cabin 
+              text-sm
+              w-[50vw]
+              focus:outline-none       
+            focus:border-accent1       
+              transition-colors duration-150"
               required
               onChange={(e) =>
                 setCredentials((prev) => ({
@@ -102,12 +115,22 @@ function Login() {
               }
             />
           </div>
-          <div>
-            <label htmlFor="password">Mot de passe :</label>
+          <div className="flex gap-[1vw] items-center">
+            <label htmlFor="password" className="font-cabin text-lg">
+              Mot de passe :
+            </label>
             <input
               type="password"
               name="password"
               id="password"
+              className="bg-color1 rounded-[10px] px-[1vw] py-[0.5vh]
+              border-t-[2px] border-l-[2px] border-b-[4px] border-r-[4px] border-color5
+              font-cabin
+              text-sm
+              w-[50vw]
+              focus:outline-none       
+            focus:border-accent1       
+              transition-colors duration-150"
               required
               onChange={(e) =>
                 setCredentials((prev) => ({
@@ -117,9 +140,23 @@ function Login() {
               }
             />
           </div>
-          <input type="submit" value="Valider" />
+          <button
+            type="submit"
+            className="bg-accent2 rounded-[10px] px-[3vw] py-[0.5vh] border-t-[2px] border-l-[2px] border-b-[4px] border-r-[4px] 
+          border-color5 font-cabin w-fit
+          transition-transform duration-100 active:scale-95"
+          >
+            Valider
+          </button>
         </form>
-        <button onClick={handleDisconnect}>Déconnexion</button>
+        <button
+          onClick={handleDisconnect}
+          className="bg-accent1 rounded-[10px] px-[3vw] py-[0.5vh] border-t-[2px] border-l-[2px] border-b-[4px] border-r-[4px] 
+          border-color5 font-cabin
+          transition-transform duration-100 active:scale-95"
+        >
+          Déconnexion
+        </button>
       </div>
       <Menu />
     </>
